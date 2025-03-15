5bf8347819bf5ff340ac1cdd70d77f811437e910
bathula Satya <satya.bathula9264@gmail.com>
2025-03-15 08:53:06 +0000

    djdjd

--- models/custdata.sql
+++ models/custdata.sql
@@ -1,4 +1,4 @@
 with cast_data as(
-    select c_custkey,c_name,c_address from snowflake_sample_data.tpch_sf1.customer
+    select c_custkey,c_custkey,c_name,c_address from snowflake_sample_data.tpch_sf1.customer
 )
 select *from cast_data
